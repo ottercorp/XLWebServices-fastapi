5d6e0710722714b7e0771f0941bfa4229b5ebee8
Bluefissure <bluefissure@foxmail.com>
2022-12-01 03:50:09 +0800

    update requirements & add color to output

--- manual_regen.py
+++ manual_regen.py
@@ -1,4 +1,10 @@
+import sys
 import asyncio
 from app.utils.tasks import *
 
-asyncio.run(regen(['dalamud', 'asset', 'plugin', 'xl']))
+
+if __name__ == '__main__':
+    print(sys.argv)
+    task_list = ['dalamud', 'asset', 'plugin', 'xivlauncher'] \
+        if len(sys.argv) == 1 else sys.argv[1:]
+    asyncio.run(regen(task_list))
