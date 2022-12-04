ad2a2e20e110c1bd028ee3fbab41bbe032c1627f
subjadeites <sublunary95@gmail.com>
2022-12-04 13:26:58 +0800

    change worker_class of gunicorn

--- gun.py
+++ gun.py
@@ -5,10 +5,6 @@
 # @Author  : subjadeites
 # @File    : gun.py
 
-import gevent.monkey
-
-gevent.monkey.patch_all()
-
 import multiprocessing
 
 debug = False
@@ -19,6 +15,6 @@ logfile = 'logs/debug.log'
 
 # 启动的进程数
 workers = multiprocessing.cpu_count()
-worker_class = 'gunicorn.workers.ggevent.GeventWorker'
+worker_class = 'uvicorn.workers.UvicornH11Worker'
 
 x_forwarded_for_header = 'X-FORWARDED-FOR'
