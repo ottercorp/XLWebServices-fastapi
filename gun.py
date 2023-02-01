def39c63dc61d3289d09d7140cfe04f95b85568f
subjadeites <sublunary95@gmail.com>
2023-02-01 11:14:39 +0800

    update: New gunicorn config

--- gun.py
+++ gun.py
@@ -12,9 +12,11 @@ loglevel = 'info'
 bind = '0.0.0.0:8080'
 pidfile = 'logs/gunicorn.pid'
 logfile = 'logs/debug.log'
+errorlog = './logs/error.log'
 
 # 启动的进程数
 workers = multiprocessing.cpu_count()
 worker_class = 'uvicorn.workers.UvicornH11Worker'
+preload_app = True
 
 x_forwarded_for_header = 'X-FORWARDED-FOR'
