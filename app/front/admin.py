83ded5a6895c9bcd92afb2c981ec9ba2034f5833
subjadeites <sublunary95@gmail.com>
2024-10-24 12:02:07 +0800

    feat: web admin to solve feedback

--- app/front/admin.py
+++ app/front/admin.py
@@ -72,7 +72,6 @@ async def front_admin_update():
 # region feedback
 @router.get('/feedback', response_class=HTMLResponse)
 async def front_admin_feedback_get(request: Request):
-    r = Redis.create_client()
     r_fb = RedisFeedBack.create_client()
     feedback_list = r_fb.keys('feedback|*')
     return_list = []
@@ -81,14 +80,60 @@ async def front_admin_feedback_get(request: Request):
         return_list.append(temp_list)
     return template.TemplateResponse("feedback_admin.html", {"request": request, "feedback_list": return_list})
 
+
+@router.get('/feedback/export', response_class=HTMLResponse)
+async def front_admin_feedback_export_get(request: Request):
+    r_fb = RedisFeedBack.create_client()
+    feedback_list = r_fb.keys('feedback|*')
+    return_dict = {}
+    for i in feedback_list:
+        dhash, plugin_name, order_id = i.replace('feedback|', '').split('|')
+        if plugin_name not in return_dict:
+            return_dict[plugin_name] = []
+        feedback = r_fb.hgetall(f'feedback|{dhash}|{plugin_name}|{order_id}')
+        return_dict[plugin_name].append({
+            "order_id": order_id,
+            "dhash": dhash,
+            "version": feedback['version'],
+            "content": feedback['content'],
+            "exception": feedback['exception'],
+            "reporter": feedback['reporter'],
+        })
+    for k, v in return_dict.items():
+        return_dict[k] = sorted(v, key=lambda x: x['order_id'], reverse=True)
+    return template.TemplateResponse("feedback_export.html", {"request": request, "export_dict": return_dict})
+
+
 @router.get('/feedback/detail/{plugin_name}/{feedback_id}', response_class=HTMLResponse)
-async def front_admin_feedback_detail_get(request: Request, plugin_name: str, feedback_id: int, dhash: str):
+async def front_admin_feedback_detail_get(request: Request, plugin_name: str, feedback_id: int, dhash: str | None = None):
     r = Redis.create_client()
     feedback = r.hgetall(f'feedback|{plugin_name}|{feedback_id}')
     if not feedback:
         raise HTTPException(status_code=404, detail="Feedback not found")
     feedback['reply_log'] = json.loads(feedback['reply_log'])
-    return template.TemplateResponse("feedback_admin_detail.html", {"request": request, "feedback": feedback})
+    return template.TemplateResponse("feedback_admin_detail.html", {"request": request, "detail": feedback})
+
+
+@router.get('/feedback/solve/{feedback_id}', response_class=RedirectResponse)
+async def front_admin_feedback_solve_post(request: Request, feedback_id: int, referer: str | None = None):
+    r_fb = RedisFeedBack.create_client()
+    feedback_list = r_fb.keys(f'feedback|*|{feedback_id}')
+    if len(feedback_list) == 1:
+        r_fb.delete(feedback_list[0])
+        if referer == "export":
+            return RedirectResponse(request.app.url_path_for('front_admin_feedback_export_get'))
+        else:
+            return RedirectResponse(request.app.url_path_for('admins._feedback_admin'))
+    elif len(feedback_list) > 1:
+        raise HTTPException(status_code=400, detail="More than one feedback found.")
+    else:
+        raise HTTPException(status_code=400, detail="No feedback found.")
+
+
+@router.post('/feedback/reply/{feedback_id}', response_class=RedirectResponse)
+async def front_admin_feedback_reply_post(request: Request, feedback_id: int, content: str):
+    raise HTTPException(status_code=404, detail="Not implemented yet.")
+
 
 # endregion
 
